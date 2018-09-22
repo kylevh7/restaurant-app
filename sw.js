59dac096c223e3f6f69fea4d1aa831986af2bbf9
unknown <kylevh7@gmail.com>
2018-09-22 12:09:53 -0400

    services workers running and caching

--- sw.js
+++ sw.js
@@ -1,54 +1,40 @@
-let CACHE_NAME = "V3";
-const cacheAssets = [
-    '/index.html',
-    '/restaurant.html',
-    '/css/styles.css',
-    '/css/styles-rest.css',
-    '/js/dbhelper.js',
-    '/js/main.js',
-    '/js/restaurant_info.js'
-];
+let CACHE_NAME = "V52";
+
 
 //install Service serviceWorker
-self.addEventListener('install', (e) =>{
+self.addEventListener('install', (e) => {
     console.log("service worker installed");
-    e.waitUntil(
-        caches
-        .open(CACHE_NAME)
-        .then(cache => {
-            console.log('caching files');
-            cache.addAll(cacheAssets);
-        })
-        .then(() => self.skipWaiting())
-    );
 });
 
 self.addEventListener('activate', e => {
     console.log('worker activated');
     //Remove old caches
     e.waitUntil(
-        caches.keys().then(cacheNames =>{
+        caches.keys().then(cacheNames => {
             return Promise.all(
-                cacheNames.map(cache =>{
-                    if(cache !== CACHE_NAME){
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
                         console.log('Service Worker: clearing old cache');
-                        return caches.delete(cache);                    }
+                        return caches.delete(cache);
+                    }
                 })
             )
         })
     );
 });
 //fetch addEventListener
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(CACHE_NAME).then(cache => {
-     return cache.match(event.request).then(response => {
-      return response || fetch(event.request)
-      .then(response => {
-        const responseClone = response.clone();
-        cache.put(event.request, responseClone);
-        })
-      })
-  })   
- );
+self.addEventListener('fetch', e=> {
+    e.respondWith(
+        fetch(e.request)
+        .then(res =>{
+            const resClone = res.clone();
+            caches
+            .open(CACHE_NAME)
+            .then(cache =>{
+                cache.put(e.request, resClone);
+            });
+            return res;
+
+        }).catch(err => caches.match(e.request).then(res => res))
+    );
 });
