d3bee3af54e840b7fdeee89093e9ab027d92556b
unknown <kylevh7@gmail.com>
2018-09-24 20:38:47 -0400

    accessability code changes made

--- js/main.js
+++ js/main.js
@@ -170,10 +170,10 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt="restaurant image"
+  image.alt=`${restaurant.name}`
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
@@ -188,6 +188,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.setAttribute('role', 'button');
   more.tabindex = '3';
   li.append(more)
 
